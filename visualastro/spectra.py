144a6b87e7ba4b9a9ea915241697bfefa66b85f7
elkogerville <elkogerville@gmail.com>
2025-05-31 17:57:22 +0200

    integrated units fully into all major plotting functions

--- visualastro/spectra.py
+++ visualastro/spectra.py
@@ -4,62 +4,68 @@ from spectral_cube import SpectralCube
 from specutils.spectra import Spectrum1D
 from specutils.fitting import fit_generic_continuum, fit_continuum
 import matplotlib.pyplot as plt
-from .plot_utils import return_stylename, save_figure_2_disk, set_axis_labels, set_plot_colors
+from .plot_utils import return_stylename, save_figure_2_disk, set_axis_labels, set_plot_colors, set_spectral_axis
 
 def plot_cube_spectra(cubes, normalize_continuum=False, plot_continuum_fit=False,
-                  fit_method='fit_generic_continuum', region=None, radial_vel=None,
-                  emission_line=None, labels=None, x_limits=None, y_limits=None,
-                  x_units=None, y_units=None, colors=None, return_spectra=False,
-                  style='astro', use_brackets=False, savefig=False, dpi=600, figsize=(6,6)):
-    c = 299792.458
+                      fit_method='fit_generic_continuum', region=None, radial_vel=None,
+                      rest_freq=None, unit=None, emission_line=None, labels=None,
+                      xlim=None, ylim=None, x_units=None, y_units=None, colors=None, return_spectra=False,
+                      style='astro', use_brackets=False, savefig=False, dpi=600, figsize=(6,6)):
+    c = 299792.458 # m/s
     spectra_dict_list = []
-    spec_normalized, continuum_fit = None, None
     colors, fit_colors = set_plot_colors(colors)
-    cubes = [cubes] if isinstance(cubes, SpectralCube) else cubes
+    cubes = [cubes] if not isinstance(cubes, list) else cubes
     style = return_stylename(style)
     with plt.style.context(style):
-        fig = plt.figure(figsize=figsize)
+        plt.figure(figsize=figsize)
         if emission_line is not None:
             plt.text(0.025, 0.95, f'{emission_line}', transform=plt.gca().transAxes)
         for i, cube in enumerate(cubes):
             spectra_dict = {}
-            wavelengths = cube.spectral_axis.to(u.micron)
-            if radial_vel is not None:
-                wavelengths /= (1 + radial_vel/c)
-            xmin = x_limits[0] if x_limits else wavelengths.value.min()
-            xmax = x_limits[1] if x_limits else wavelengths.value.max()
-            mask = (wavelengths.value > xmin) & (wavelengths.value < xmax)
+            spec_normalized, continuum_fit = None, None
+            spectral_axis = set_spectral_axis(cube, unit)
             spectrum = cube.mean(axis=(1,2))
-            spectra_dict['wavelength'] = wavelengths
+
+            if radial_vel is not None:
+                if spectral_axis.unit.is_equivalent(u.Hz):
+                    spectral_axis *= (1 + radial_vel / c)
+                else:
+                    spectral_axis /= (1 + radial_vel / c)
+            if rest_freq is not None:
+                spectral_axis = c*u.km/u.s * (rest_freq - spectral_axis) / rest_freq
+
+            xmin = xlim[0] if xlim is not None else spectral_axis.value.min()
+            xmax = xlim[1] if xlim is not None else spectral_axis.value.max()
+            mask = (spectral_axis.value > xmin) & (spectral_axis.value < xmax)
+            spectra_dict['wavelength'] = spectral_axis
             spectra_dict['flux'] = spectrum
             label = labels[i] if (labels is not None and i < len(labels)) else None
             if normalize_continuum != plot_continuum_fit:
-                spectrum1d = Spectrum1D(flux=spectrum, spectral_axis=wavelengths)
+                spectrum1d = Spectrum1D(flux=spectrum, spectral_axis=spectral_axis)
                 with warnings.catch_warnings():
                     warnings.simplefilter('ignore')
                     if fit_method=='fit_continuum':
                         fit = fit_continuum(spectrum1d, window=region)
                     else:
                         fit = fit_generic_continuum(spectrum1d)
-                continuum_fit = fit(wavelengths)
+                continuum_fit = fit(spectral_axis)
                 spec_normalized = spectrum1d / continuum_fit
                 if normalize_continuum:
                     plt.plot(spec_normalized.spectral_axis[mask], spec_normalized.flux[mask],
                              color=colors[i%len(colors)], label=label)
             if not normalize_continuum:
-                plt.plot(wavelengths[mask], spectrum[mask], color=colors[i%len(colors)], label=label)
+                plt.plot(spectral_axis[mask], spectrum[mask], color=colors[i%len(colors)], label=label)
             if plot_continuum_fit:
-                plt.plot(wavelengths[mask], continuum_fit[mask], color=fit_colors[i%len(fit_colors)])
+                plt.plot(spectral_axis[mask], continuum_fit[mask], color=fit_colors[i%len(fit_colors)])
             spectra_dict['spec_norm'] = spec_normalized
             spectra_dict['continuum_fit'] = continuum_fit
             spectra_dict_list.append(spectra_dict)
-        x_min = x_limits[0] if x_limits is not None else wavelengths.value.min()
-        x_max = x_limits[1] if x_limits is not None else wavelengths.value.max()
-        plt.xlim(x_min, x_max)
-        if y_limits is not None:
-            plt.ylim(y_limits[0], y_limits[1])
 
-        set_axis_labels(wavelengths, cubes[0], x_units, y_units, use_brackets=use_brackets)
+        plt.xlim(xmin, xmax)
+        if ylim is not None:
+            plt.ylim(ylim[0], ylim[1])
+
+        set_axis_labels(spectral_axis, spectrum, x_units, y_units, use_brackets=use_brackets)
 
         if labels is not None:
             plt.legend()
