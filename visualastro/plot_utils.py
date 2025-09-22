4288c2f1f82f436094ba269bc90ffeb1f62a833c
elkogerville <elkogerville@gmail.com>
2025-09-22 14:31:27 -0400

    refactoring architecture such that each module has its own dedicated utils module, and moving general functions to either numerical_utils.py or plot_utils.py

--- visualastro/plot_utils.py
+++ visualastro/plot_utils.py
@@ -2,8 +2,7 @@ import os
 from functools import partial
 import numpy as np
 from astropy.visualization import AsinhStretch, ImageNormalize
-from astropy import units as u
-from astropy.units import spectral
+
 import matplotlib.style as mplstyle
 import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
@@ -12,57 +11,57 @@ from matplotlib.patches import Circle, Ellipse
 from regions import PixCoord, EllipsePixelRegion
 from .visual_classes import DataCube
 
-def return_cube_data(cube):
-    if isinstance(cube, DataCube):
-        cube = cube.data
-    return cube
+# def return_cube_data(cube):
+#     if isinstance(cube, DataCube):
+#         cube = cube.data
+#     return cube
 
-def check_is_array(data):
-    '''
-    Ensure array input is np.ndarray.
-    Parameters
-    ––––––––––
-    data : np.ndarray or DataCube
-        Array or DataCube object.
-    Returns
-    –––––––
-    data : np.ndarray
-        Array or 'data' component of DataCube.
-    '''
-    if isinstance(data, DataCube):
-        data = data.value
+# def check_is_array(data):
+#     '''
+#     Ensure array input is np.ndarray.
+#     Parameters
+#     ––––––––––
+#     data : np.ndarray or DataCube
+#         Array or DataCube object.
+#     Returns
+#     –––––––
+#     data : np.ndarray
+#         Array or 'data' component of DataCube.
+#     '''
+#     if isinstance(data, DataCube):
+#         data = data.value
 
-    return np.asarray(data)
+#     return np.asarray(data)
 
-def shift_by_radial_vel(spectral_axis, radial_vel):
-    '''
-    Shift spectral axis by a radial velocity.
-    Parameters
-    ––––––––––
-    spectral_axis : astropy.units.Quantity
-        The spectral axis to shift. Can be in frequency or wavelength units.
-    radial_vel : float or None
-        Radial velocity in km/s (astropy units are not needed). Positive values
-        correspond to a redshift (moving away). If None, no shift is applied.
-    Returns
-    –––––––
-    shifted_axis : astropy.units.Quantity
-        The spectral axis shifted according to the given radial velocity.
-        If the input is in frequency units, the relativistic Doppler
-        formula for frequency is applied; otherwise, the formula for
-        wavelength is applied.
-    '''
-    # speed of light in km/s in vacuum
-    c = 299792.458 # [km/s]
-    if radial_vel is not None:
-        # if spectral axis in units of frequency
-        if spectral_axis.unit.is_equivalent(u.Hz):
-            spectral_axis /= (1 - radial_vel / c)
-        # if spectral axis in units of wavelength
-        else:
-            spectral_axis /= (1 + radial_vel / c)
+# def shift_by_radial_vel(spectral_axis, radial_vel):
+#     '''
+#     Shift spectral axis by a radial velocity.
+#     Parameters
+#     ––––––––––
+#     spectral_axis : astropy.units.Quantity
+#         The spectral axis to shift. Can be in frequency or wavelength units.
+#     radial_vel : float or None
+#         Radial velocity in km/s (astropy units are not needed). Positive values
+#         correspond to a redshift (moving away). If None, no shift is applied.
+#     Returns
+#     –––––––
+#     shifted_axis : astropy.units.Quantity
+#         The spectral axis shifted according to the given radial velocity.
+#         If the input is in frequency units, the relativistic Doppler
+#         formula for frequency is applied; otherwise, the formula for
+#         wavelength is applied.
+#     '''
+#     # speed of light in km/s in vacuum
+#     c = 299792.458 # [km/s]
+#     if radial_vel is not None:
+#         # if spectral axis in units of frequency
+#         if spectral_axis.unit.is_equivalent(u.Hz):
+#             spectral_axis /= (1 - radial_vel / c)
+#         # if spectral axis in units of wavelength
+#         else:
+#             spectral_axis /= (1 + radial_vel / c)
 
-    return spectral_axis
+#     return spectral_axis
 
 def return_stylename(style):
     '''
@@ -171,65 +170,65 @@ def return_imshow_norm(vmin, vmax, norm):
 
     return norm_map[norm]
 
-def return_cube_slice(cube, idx):
-    '''
-    Return a slice of a data cube along the first axis.
-    Parameters
-    ––––––––––
-    cube : np.ndarray
-        Input data cube, typically with shape (T, N, ...) where T is the first axis.
-    idx : int or list of int
-        Index or indices specifying the slice along the first axis:
-        - i -> returns 'cube[i]'
-        - [i] -> returns 'cube[i]'
-        - [i, j] -> returns 'cube[i:j+1].sum(axis=0)'
-    Returns
-    –––––––
-    cube : np.ndarray
-        Sliced cube with shape (N, ...).
-    '''
-    cube = return_cube_data(cube)
-    # if index is integer
-    if isinstance(idx, int):
-        return cube[idx]
-    # if index is list of integers
-    elif isinstance(idx, list):
-        # list of len 1
-        if len(idx) == 1:
-            return cube[idx[0]]
-        # list of len 2
-        elif len(idx) == 2:
-            start, end = idx
-            return cube[start:end+1].sum(axis=0)
-    raise ValueError("'idx' must be an int or a list of one or two integers")
+# def return_cube_slice(cube, idx):
+#     '''
+#     Return a slice of a data cube along the first axis.
+#     Parameters
+#     ––––––––––
+#     cube : np.ndarray
+#         Input data cube, typically with shape (T, N, ...) where T is the first axis.
+#     idx : int or list of int
+#         Index or indices specifying the slice along the first axis:
+#         - i -> returns 'cube[i]'
+#         - [i] -> returns 'cube[i]'
+#         - [i, j] -> returns 'cube[i:j+1].sum(axis=0)'
+#     Returns
+#     –––––––
+#     cube : np.ndarray
+#         Sliced cube with shape (N, ...).
+#     '''
+#     cube = return_cube_data(cube)
+#     # if index is integer
+#     if isinstance(idx, int):
+#         return cube[idx]
+#     # if index is list of integers
+#     elif isinstance(idx, list):
+#         # list of len 1
+#         if len(idx) == 1:
+#             return cube[idx[0]]
+#         # list of len 2
+#         elif len(idx) == 2:
+#             start, end = idx
+#             return cube[start:end+1].sum(axis=0)
+#     raise ValueError("'idx' must be an int or a list of one or two integers")
 
-def extract_spectral_axis(cube, unit=None):
-    '''
-    Extract the spectral axis from a data cube and optionally
-    convert it to a specified unit.
-    Parameters
-    ––––––––––
-    cube : SpectralCube
-        The input spectral data cube.
-    unit : astropy.units.Unit, optional
-        Desired unit for the spectral axis. If None, the axis
-        is returned in its native units.
-    Returns
-    –––––––
-    spectral_axis : Quantity
-        The spectral axis of the cube, optionally converted
-        to the specified unit.
-    '''
-    axis = cube.spectral_axis
-    # return axis if unit is None
-    if unit is None:
-        return axis
-    # if a unit is specified, attempt to
-    # convert axis to those units
-    try:
-        return axis.to(unit, equivalencies=spectral())
-    except u.UnitConversionError:
-        raise ValueError(f"Cannot convert spectral axis from {axis.unit} to {unit}")
+# def extract_spectral_axis(cube, unit=None):
+#     '''
+#     Extract the spectral axis from a data cube and optionally
+#     convert it to a specified unit.
+#     Parameters
+#     ––––––––––
+#     cube : SpectralCube
+#         The input spectral data cube.
+#     unit : astropy.units.Unit, optional
+#         Desired unit for the spectral axis. If None, the axis
+#         is returned in its native units.
+#     Returns
+#     –––––––
+#     spectral_axis : Quantity
+#         The spectral axis of the cube, optionally converted
+#         to the specified unit.
+#     '''
+#     axis = cube.spectral_axis
+#     # return axis if unit is None
+#     if unit is None:
+#         return axis
+#     # if a unit is specified, attempt to
+#     # convert axis to those units
+#     try:
+#         return axis.to(unit, equivalencies=spectral())
+#     except u.UnitConversionError:
+#         raise ValueError(f"Cannot convert spectral axis from {axis.unit} to {unit}")
 
 def get_spectral_slice_value(spectral_axis, idx):
     '''
