53f22a53f0c849c4c39e74dbba480ca4e9235c9e
elkogerville <elkogerville@gmail.com>
2025-12-06 16:19:44 +0100

    changed __init__ to use absolute imports

--- src/visualastro/__init__.py
+++ src/visualastro/__init__.py
@@ -1,14 +1,16 @@
 # Core Classes
-from .visual_classes import DataCube, ExtractedSpectrum, FitsFile
+from visualastro.DataCube import DataCube
+from visualastro.ExtractedSpectrum import ExtractedSpectrum
+from visualastro.FitsFile import FitsFile
 
 # Submodules
-from .data_cube import *
-from .data_cube_utils import *
-from .io import *
-from .numerical_utils import *
-from .plotting import *
-from .plot_utils import *
-from .spectra import *
-from .spectra_utils import *
-from .va_config import va_config
-from .visual_plots import *
+from visualastro.data_cube import *
+from visualastro.data_cube_utils import *
+from visualastro.io import *
+from visualastro.numerical_utils import *
+from visualastro.plotting import *
+from visualastro.plot_utils import *
+from visualastro.spectra import *
+from visualastro.spectra_utils import *
+from visualastro.va_config import va_config
+from visualastro.visual_plots import *
