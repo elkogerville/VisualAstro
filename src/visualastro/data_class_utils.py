7b31e013b700ad56e7da12593ed369042e446632
elkogerville <elkogerville@gmail.com>
2025-12-10 12:13:53 +0100

    def validate_type to ensure an object is correct type

--- src/visualastro/data_class_utils.py
+++ src/visualastro/data_class_utils.py
@@ -41,9 +41,9 @@ def get_common_units(objs):
     if not np.iterable(objs) or isinstance(objs, (Header, Quantity)):
         objs = [objs]
 
+    # create unique set of each unit
     units = set()
     for i, obj in enumerate(objs):
-        # create unique set of each unit
         if isinstance(obj, Quantity):
             units.add(obj.unit)
         elif isinstance(obj, Header) and 'BUNIT' in obj:
@@ -122,3 +122,45 @@ def update_BUNIT(unit, header, primary_header):
         new_hdr = None
 
     return new_hdr
+
+
+def validate_type(
+    data, types, default=None, allow_none=True, name='data'
+):
+    '''
+    Validate that `data` is an instance of one of the allowed types.
+
+    Parameters
+    ––––––––––
+    data : object
+        The object to validate.
+    types : type or tuple of types
+        A type or tuple of types that `data` is allowed to be.
+        Ex: Quantity, (Quantity), or (Quantity, SpectralCube)
+    default : object, optional, default=None
+            Value to return if `data` is None. Use this to provide
+            a default instance when None is passed.
+    allow_none : bool, default=True
+            If True, None is a valid input. If False, None will raise TypeError.
+    name : str
+        Name of object for error message.
+
+    Raises
+    ––––––
+    TypeError
+        If `data` is not an instance of any of the types in `types`.
+    '''
+    if data is None and allow_none:
+        return default
+
+    # make iterable
+    if not isinstance(types, tuple):
+        types = (types,)
+
+    if not isinstance(data, types):
+        allowed = ', '.join(t.__name__ for t in types)
+        raise TypeError(
+            f"'{name}' must be one of: {allowed}; got {type(data).__name__}."
+        )
+
+    return data
