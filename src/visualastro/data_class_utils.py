61fda88a791826bbd90ab03c90d0f679e86bc8f9
elkogerville <elkogerville@gmail.com>
2025-12-10 12:11:43 +0100

    def get_common_units to extract unit from a list of objects and validate that they match

--- src/visualastro/data_class_utils.py
+++ src/visualastro/data_class_utils.py
@@ -11,8 +11,58 @@ Dependencies:
 
 from astropy.io.fits import Header
 from astropy.time import Time
-from astropy.units import Unit
+from astropy.units import Quantity, Unit, UnitsError
 import numpy as np
+
+
+def get_common_units(objs):
+    '''
+    Extract units of each object in objs
+    and validate that units match.
+
+    Parameters
+    ––––––––––
+    obj : array-like
+        A single object or list/array of objects with unit data.
+        Can be Quantities, Headers with 'BUNIT', or a mix of both.
+
+    Returns
+    –––––––
+    None
+        If no units are present.
+    astropy.units.Unit
+        If units are present and are consistent.
+
+    Raises
+    ––––––
+    UnitsError
+        If units exist and do not match, or if BUNIT is invalid.
+    '''
+    if not np.iterable(objs) or isinstance(objs, (Header, Quantity)):
+        objs = [objs]
+
+    units = set()
+    for i, obj in enumerate(objs):
+        # create unique set of each unit
+        if isinstance(obj, Quantity):
+            units.add(obj.unit)
+        elif isinstance(obj, Header) and 'BUNIT' in obj:
+            try:
+                units.add(Unit(obj['BUNIT'])) # type: ignore
+            except Exception as e:
+                raise UnitsError(
+                    f'Invalid BUNIT in header at index {i}: '
+                    f"'{obj['BUNIT']}' ({e})"
+                )
+    # raise error if more than one unit found
+    if len(units) > 1:
+        raise UnitsError(
+            f'Inconsistent units found: {units}'
+        )
+
+    # return either single unit or None
+    return next(iter(units), None)
+
 
 def log_history(header, message):
     '''
@@ -27,6 +77,7 @@ def log_history(header, message):
     log = f'{timestamp} {message}'
 
     header.add_history(log)
+
 
 def update_BUNIT(unit, header, primary_header):
     '''
