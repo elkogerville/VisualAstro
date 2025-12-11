b616a8d2096c472d8908eff2010a52ec624c5852
elkogerville <elkogerville@gmail.com>
2025-12-11 22:47:25 +0100

    cleaned up fits_utils.py

--- src/visualastro/fits_utils.py
+++ src/visualastro/fits_utils.py
@@ -15,21 +15,6 @@ from astropy.units import Unit
 import numpy as np
 
 
-def _log_history(header, message):
-    '''
-    Add `HISTORY` entry to header.
-
-    Parameters
-    ––––––––––
-    header : astropy.Header
-    message : str
-    '''
-    timestamp = Time.now().isot
-    log = f'{timestamp} {message}'
-
-    header.add_history(log)
-
-
 def update_header_key(key, value, header, primary_header):
     '''
     Update header(s) in place with a new key-value pair.
@@ -130,3 +115,72 @@ def with_updated_header_key(key, value, header, primary_header):
     # case 3: no valid Header
     else:
         return None
+
+
+def _get_history(header):
+    '''
+    Get `HISTORY` cards from a Header as a list.
+
+    Parameters
+    ––––––––––
+    header : Header
+        Fits Header with `HISTORY` cards.
+
+    Returns
+    –––––––
+    list or None :
+        all `HISTORY` cards or None if no entries.
+    '''
+    if not isinstance(header, Header) or "HISTORY" not in header:
+        return None
+
+    history = header["HISTORY"]
+
+    if isinstance(history, str):
+        return [history]
+
+    return list(history) # type: ignore
+
+
+def _log_history(header, message):
+    '''
+    Add `HISTORY` entry to header.
+
+    Parameters
+    ––––––––––
+    header : astropy.Header
+    message : str
+    '''
+    timestamp = Time.now().isot
+    log = f'{timestamp} {message}'
+
+    header.add_history(log)
+
+
+def _transfer_history(header1, header2):
+    '''
+    Trasfer `HISTORY` cards from one
+    header to another. This is not a
+    destructive action.
+
+    Parameters
+    ––––––––––
+    header1 : Header
+        Fits Header with `HISTORY` cards to send.
+    header2 : Header
+        Fits Header to copy `HISTORY` cards to.
+
+    Returns
+    –––––––
+    header2 : Header
+        Fits Header with updated `HISTORY`.
+    '''
+    # get logs from header 1
+    hdr1_history = _get_history(header1)
+
+    # add logs to header 2
+    if hdr1_history is not None:
+        for history in hdr1_history:
+            header2.add_history(history)
+
+    return header2
