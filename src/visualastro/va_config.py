81227ebdd9b557a37b94f63e9f08ae40c7a51979
elkogerville <elkogerville@gmail.com>
2025-12-23 18:08:12 -0500

    renamed va_config.interpolate to .curve_fit_interpolate

--- src/visualastro/va_config.py
+++ src/visualastro/va_config.py
@@ -165,7 +165,7 @@ class VAConfig:
 
         # gaussian fitting params
         self.gaussian_model = 'gaussian'
-        self.interpolate = False
+        self.curve_fit_interpolate = False
         self.return_gaussian_fit_parameters = False
         self.print_gaussian_values = True
         self.curve_fit_method = 'trf'
