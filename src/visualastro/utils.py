0b25102afe3792057c4ffe25ea6d3528804e4f22
elkogerville <elkogerville@gmail.com>
2026-01-16 02:16:57 +0100

    moved _unwrap_if_single to utils.py to avoid circular imports

---
+++ src/visualastro/utils.py
@@ -0,0 +1,31 @@
+'''
+Author: Elko Gerville-Reache
+Date Created: 2026-01-16
+Date Modified: 2026-01-16
+Description:
+    General utility functions.
+'''
+
+
+def _unwrap_if_single(array):
+    '''
+    Unwrap an array-like object if it contains exactly one element.
+
+    If the input has length 1, the sole element is returned.
+    Otherwise, the input is returned unchanged. This is primarily
+    intended for user-facing APIs that return either a single object
+    or a collection depending on the number of results.
+
+    Parameters
+    ----------
+    array : Sequence[T]
+        A sequence-like object supporting `len()` and indexing.
+        Must have at least one element.
+
+    Returns
+    -------
+    T or Sequence[T]
+        The sole element if `len(array) == 1`, otherwise the original
+        input sequence.
+    '''
+    return array[0] if len(array) == 1 else array
