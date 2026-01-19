911a66031a878e1220f39afa296f4bb24655cde3
elkogerville <elkogerville@gmail.com>
2026-01-19 15:23:33 +0100

    fix __init__ imports

--- src/visualastro/__init__.py
+++ src/visualastro/__init__.py
@@ -7,7 +7,11 @@ from visualastro.SpectrumPlus import SpectrumPlus
 from visualastro.data_cube import *
 from visualastro.data_cube_utils import *
 from visualastro.io import *
-from visualastro.numerical_utils import *
+from visualastro.numerical_utils import (
+    get_data, get_value, to_array, to_list,
+    compute_density_kde, shift_by_radial_vel,
+    interpolate, finite, mask_finite, mask_within_range
+)
 from visualastro.plotting import *
 from visualastro.plot_utils import *
 from visualastro.spectra import *
