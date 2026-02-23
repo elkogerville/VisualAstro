7382867bead68429e10fc60bccafd67629c5afed
elkogerville <elkogerville@gmail.com>
2026-02-23 17:16:51 +0100

    def _type_name to replace type(obj).__name__

--- src/visualastro/utils.py
+++ src/visualastro/utils.py
@@ -7,6 +7,9 @@ Description:
 '''
 
 
+from typing import Any
+
+
 def _unwrap_if_single(array):
     '''
     Unwrap an array-like object if it contains exactly one element.
@@ -29,3 +32,24 @@ def _unwrap_if_single(array):
         input sequence.
     '''
     return array[0] if len(array) == 1 else array
+
+
+def _type_name(obj: Any) -> str:
+    """
+    Get a readable, string representation of an object's type.
+
+    This is used internally for error messages, and is an
+    alias for:
+        ``type(obj).__name__``.
+
+    Parameters
+    ----------
+    obj : Any
+        Object to return type from.
+
+    Returns
+    -------
+    str :
+        String of the object type.
+    """
+    return type(obj).__name__
