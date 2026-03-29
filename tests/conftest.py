dc3c52553efbf1c516dff1e8908a84e25acc6796
elkogerville <elkogerville@gmail.com>
2026-03-29 21:30:20 +0200

    cleanup conftest test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,6 @@ def generate_test_cube() -> fits.PrimaryHDU:
     -------
     hdu : astropy.io.fits.PrimaryHDU
         HDU object containing the data and header extensions.
-
     """
     data = np.random.randn(10, 50, 50) + 5
 
@@ -35,23 +34,36 @@ def generate_test_cube() -> fits.PrimaryHDU:
 
     assert isinstance(data, np.ndarray)
     assert isinstance(header, fits.Header)
+    assert isinstance(hdu, fits.PrimaryHDU)
 
     return hdu
 
 
 @pytest.fixture
-def generate_test_image():
-    """Generate a 2D test image."""
+def generate_test_image() -> fits.PrimaryHDU:
+    """
+    Generate a 2D test image along with valid WCS.
+
+    Returns
+    -------
+    hdu : astropy.io.fits.PrimaryHDU
+        HDU object containing the data and header extensions.
+    """
     data = np.random.randn(100, 100) + 10
 
     wcs = WCS(naxis=2)
     wcs.wcs.crpix = [50, 50]
     wcs.wcs.cdelt = [1e-5, 1e-5]
     wcs.wcs.crval = [83.866, -69.270]
-    wcs.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+    wcs.wcs.ctype = ['RA---TAN', 'DEC--TAN']
 
     header = wcs.to_header()
     header['BUNIT'] = 'Jy/beam'
 
     hdu = fits.PrimaryHDU(data=data, header=header)
+
+    assert isinstance(data, np.ndarray)
+    assert isinstance(header, fits.Header)
+    assert isinstance(hdu, fits.PrimaryHDU)
+
     return hdu
