debd70a7724fc032231465c5a9de612d37a51a82
elkogerville <elkogerville@gmail.com>
2026-04-01 16:12:47 +0200

    pytest fixtures generate_test_fitsfile/datacube/spectralcube

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 from astropy.io import fits
+import astropy.units as u
 from astropy.wcs import WCS
 import numpy as np
 import pytest
 from spectral_cube import SpectralCube
+
+from visualastro.dataclasses.datacube import DataCube
+from visualastro.dataclasses.fitsfile import FitsFile
 
 
 @pytest.fixture
@@ -13,7 +17,7 @@ def generate_test_cube() -> fits.PrimaryHDU:
 
     Returns
     -------
-    hdu : astropy.io.fits.PrimaryHDU
+    hdu : fits.PrimaryHDU
         HDU object containing the data and header extensions.
     """
     data = np.random.randn(10, 50, 50) + 5
@@ -41,25 +45,13 @@ def generate_test_cube() -> fits.PrimaryHDU:
 
 
 @pytest.fixture
-def generate_test_spectralcube(generate_test_cube) -> SpectralCube:
-    """
-    Generate test SpectralCube
-    """
-    hdu = generate_test_cube
-    cube = SpectralCube.read(hdu)
-
-    assert isinstance(cube, SpectralCube)
-
-    return cube
-
-@pytest.fixture
 def generate_test_image() -> fits.PrimaryHDU:
     """
     Generate a 2D test image along with valid WCS.
 
     Returns
     -------
-    hdu : astropy.io.fits.PrimaryHDU
+    hdu : fits.PrimaryHDU
         HDU object containing the data and header extensions.
     """
     data = np.random.randn(100, 100) + 10
@@ -80,3 +72,43 @@ def generate_test_image() -> fits.PrimaryHDU:
     assert isinstance(hdu, fits.PrimaryHDU)
 
     return hdu
+
+
+@pytest.fixture
+def generate_test_spectralcube(generate_test_cube) -> SpectralCube:
+    """Generate test SpectralCube"""
+    hdu = generate_test_cube
+    cube = SpectralCube.read(hdu)
+
+    assert isinstance(cube, SpectralCube)
+
+    return cube
+
+
+@pytest.fixture
+def generate_test_datacube(generate_test_cube) -> DataCube:
+    """Generate test DataCube"""
+    hdu = generate_test_cube
+    header = hdu.header
+    unit = u.Unit(header['BUNIT'])
+    data = hdu.data * unit
+    cube = DataCube(data=data, header=header)
+
+    assert isinstance(cube, DataCube)
+
+    return cube
+
+
+@pytest.fixture
+def generate_test_fitsfile(generate_test_image) -> FitsFile:
+    """Generate test FitsFile"""
+
+    hdu = generate_test_image
+    header = hdu.header
+    unit = u.Unit(header['BUNIT'])
+    data = hdu.data * unit
+    fitsfile = FitsFile(data=data, header=header)
+
+    assert isinstance(fitsfile, FitsFile)
+
+    return fitsfile
