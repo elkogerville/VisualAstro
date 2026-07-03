2ede3d1d54803b8f309a4c85485e0fb5bec42cbe
elkogerville <elkogerville@gmail.com>
2026-07-03 02:47:50 +0200

    def _style_context and mv _get_stylepath to style.py

--- src/visualastro/plotting/core/utils.py
+++ src/visualastro/plotting/core/utils.py
@@ -109,67 +109,6 @@ def style(name: str | _Unset = _UNSET, *additional_styles, rc: dict | None = Non
         yield
 
 
-def _get_stylepath(style: str | list[str]) -> str | list[str]:
-    """
-    Returns the path to a visualastro mpl stylesheet for
-    consistent plotting parameters.
-
-    Matplotlib styles are also allowed (ex: 'classic').
-
-    To add custom user defined mpl sheets, add files in:
-    VisualAstro/visualastro/stylelib/
-    Ensure the stylesheet follows the naming convention:
-        `mystylesheet.mplstyle`
-
-    If a style is unable to load due to missing fonts
-    or other errors, `config.style_fallback` is used.
-
-    Parameters
-    ----------
-    style : str
-        Name of the mpl stylesheet without the extension.
-        ex: 'astro'
-
-    Returns
-    -------
-    style_path : str
-        Path to matplotlib stylesheet.
-    """
-    # if style is a default matplotlib stylesheet
-    if style in mplstyle.available:
-        return style
-
-    if isinstance(style, list):
-        if all(s in mplstyle.available for s in style):
-            return style
-        else:
-            raise ValueError(
-                'Invalid styles! To check available styles, '
-                "run `plt.style.available`."
-            )
-
-    # if style is a visualastro stylesheet
-    stylelib = files('visualastro').joinpath('stylelib')
-    base_style = style.split('_')[0] if '_' in style else style
-    style_path = stylelib.joinpath(f'{base_style}.mplstyle')
-
-    # ensure that style works on computer, otherwise return default style
-    try:
-        with plt.style.context(str(style_path)):
-            # pass if can load style successfully on computer
-            pass
-        return str(style_path)
-    except Exception as e:
-        warnings.warn(
-            f"[visualastro] Could not apply style '{style}' ({e}). "
-            f"Falling back to '{config.style_fallback}' style.",
-            stacklevel=2
-        )
-        style = config.style_fallback
-        base_style = style.split('_')[0] if '_' in style else style
-        return str(stylelib.joinpath(f'{base_style}.mplstyle'))
-
-
 def apply_style_modifiers(ax, style: str):
     """
     Apply programmatic style modifiers based on underscore-separated suffixes.
