5150a6a07fe9c7c4efb4375a8c8e8092556544ea
elkogerville <elkogerville@gmail.com>
2026-07-05 02:29:21 +0200

    refactored get_cmap

--- src/visualastro/plotting/core/colors.py
+++ src/visualastro/plotting/core/colors.py
@@ -10,6 +10,7 @@ from collections.abc import Sequence
 from dataclasses import dataclass, fields
 import operator as op
 
+import cmasher
 from colorspacious import cspace_convert
 import colorsys
 from typing import Literal, TypeAlias
@@ -302,7 +303,9 @@ def _get_colors(
 
 def get_cmap(
     cmap: mcolors.Colormap | str | int,
-    bad_color: ColorType | None = None
+    cmap_range: tuple[float, float] = (0, 1),
+    bad_color: ColorType | None = None,
+    N: int | None = None
 ) -> mcolors.Colormap:
     """
     Retrieve a colormap by name or return the input colormap.
@@ -313,8 +316,17 @@ def get_cmap(
         Colormap object or string name. If a string, attempts lookup in VISUALASTRO_CMAPS
         registry before falling back to matplotlib's colormap registry.
         If an `int`, returns `tol_colors.rainbow_discrete(colors)`.
+    cmap_range : tuple[float, float], optional, default=(0, 1)
+        The normalized range of the colormap. By default, is `(0,1)`,
+        meaning the returned colormap has its entire range. Ignored
+        if `cmap` is an `int`.
     bad_color : ColorType | None, optional, default=None
         Bad data color (`bad_color`). If None, leaves the colormap unchanged.
+    N : int | None, optional, default=None
+        Number of discrete color segments to sample from the sub-range
+        defined by `cmap_range`. If None, retains all colors within that
+        range (continuous colormap). Ignored if `cmap_range` is `(0, 1)`
+        or if `cmap` is an `int`.
 
     Returns
     -------
@@ -335,17 +347,21 @@ def get_cmap(
         new_cmap.set_bad(color=color)
         return new_cmap
 
-    if isinstance(cmap, str):
-        cmap_name = cmap.removesuffix('_r')
-        cm = VISUALASTRO_CMAPS.get(cmap_name, None)
-        if cm is not None:
-            cm = cm.reversed() if cmap.endswith('_r') else cm
-            return set_bad_color(cm, bad_color)
+    if len(cmap_range) != 2:
+        raise ValueError(
+            'cmap_range must be a tuple[min, max]!'
+        )
 
     if isinstance(cmap, int):
         return set_bad_color(tc.rainbow_discrete(cmap), bad_color)
 
-    return set_bad_color(plt.get_cmap(cmap), bad_color)
+    out_cmap = set_bad_color(plt.get_cmap(cmap), bad_color)
+    if cmap_range[0] == 0 and cmap_range[1] == 1:
+        return out_cmap
+
+    return cmasher.get_sub_cmap(
+        out_cmap, cmap_range[0], cmap_range[1], N=N
+    )
 
 
 def create_cmap(
