06e4dd49aafadcd0a85831aaa8795ec3b9b1bb21
elkogerville <elkogerville@gmail.com>
2026-07-12 18:39:04 +0200

    sort config.style_available

--- src/visualastro/__init__.py
+++ src/visualastro/__init__.py
@@ -245,7 +245,7 @@ def _register_styles():
     styledict = {}
     style_root = Path(stylelib)
 
-    config.style_available = list({p.stem for p in style_root.rglob('*.mplstyle')})
+    config.style_available = sorted(list({p.stem for p in style_root.rglob('*.mplstyle')}))
 
     # create set of each valid parent directory to a .mplstyle in stylelib
     dirs = {p.parent for p in style_root.rglob('*.mplstyle')}
