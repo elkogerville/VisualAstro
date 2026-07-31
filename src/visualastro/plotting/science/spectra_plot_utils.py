3b340a8af0253d20a03b316532210309332a34b6
elkogerville <elkogerville@gmail.com>
2026-07-31 16:50:00 -0400

    added fontsize to spectral_axis_label

--- src/visualastro/plotting/science/spectra_plot_utils.py
+++ src/visualastro/plotting/science/spectra_plot_utils.py
@@ -365,7 +365,8 @@ def spectral_axis_label(
     text_loc: tuple[float, float] | _Unset = _UNSET,
     text_color: ColorType | _Unset = _UNSET,
     highlight: bool | _Unset = _UNSET,
-    unit_fmt: str | _Unset = _UNSET
+    unit_fmt: str | _Unset = _UNSET,
+    fontsize: float | _Unset = _UNSET
 ) -> None:
     """
     Add a label indicating the spectral coordinate of a slice.
@@ -434,6 +435,7 @@ def spectral_axis_label(
     text_loc = _resolve_default(text_loc, config.text_loc)
     text_color = _resolve_default(text_color, config.text_color)
     highlight = _resolve_default(highlight, config.highlight)
+    fontsize = _resolve_default(fontsize, config.fontsizes.resolve('text'))
 
     spectral_axis = _get_spectral_axis(spectral_axis)
     if spectral_axis is None:
@@ -461,7 +463,10 @@ def spectral_axis_label(
         bbox = dict(facecolor='white', edgecolor='w') if highlight else None
         ax.text(
             text_loc[0], text_loc[1], slice_label,
-            transform=ax.transAxes, color=text_color, bbox=bbox
+            transform=ax.transAxes,
+            color=text_color,
+            bbox=bbox,
+            fontsize=fontsize,
         )
 
 
